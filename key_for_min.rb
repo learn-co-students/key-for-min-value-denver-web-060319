35ad4fc9278c235a9d33b6776f420a47695a8e2f
SwanHub <jdprince555@gmail.com>
2019-04-21 13:40:37 -0600

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,32 @@
 # prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
+# Given a hash with numeric values, RETURN the KEY for the SMALLEST VALUE
+# if hash = [] return nil.
+
 
 def key_for_min_value(name_hash)
 
+  if name_hash == {}
+    return nil
+
+  else
+
+    min_value_storage = [] # initial value for min.
+    min_key_storage = [] # initial value for min.
+
+    name_hash.each do |key, value| # go through each value and push the lowest.
+
+      if min_value_storage == []
+        min_value_storage.push(value)
+        min_key_storage.push(key)
+
+      elsif value < min_value_storage[0]
+        min_value_storage.replace([value])
+        min_key_storage.replace([key])
+
+      end # end of if statement using .each ## we now have an actual min.
+
+    end # end of each statement.
+
+    return min_key_storage[0]
+  end
 end
