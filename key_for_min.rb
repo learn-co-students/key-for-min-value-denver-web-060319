d1618ce59b65c7d567ca6c6c73648d3f2e1b0414
Rj <rikrdo.ortega@gmail.com>
2019-05-18 00:33:36 -0600

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,25 +4,34 @@
 def key_for_min_value(name_hash)
     new_key = ""
     to_check = 0
-    
     name_hash.each do |key,value|
         if value > to_check
             new_key = key
             to_check = value
         end
     end
-    
     name_hash.each do |key,value|
         if value < to_check
             new_key = key
             to_check = value
         end
     end
-    
     if new_key.empty?
         return nil
     else
         new_key.to_sym
     end
-
 end
+
+# def key_for_min_value(hash)
+#     lowest_key = nil
+#     lowest_value = nil
+#     hash.each do |k, v|
+#       if lowest_value == nil || v < lowest_value
+#         lowest_value = v
+#         lowest_key = k
+#       end
+#     end
+#     lowest_key
+# end
+  
