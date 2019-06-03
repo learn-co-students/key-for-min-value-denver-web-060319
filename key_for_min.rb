7abb7e277e4581a02234473e67edc7468a11e301
little-Dave <17536424+little-Dave@users.noreply.github.com>
2019-06-02 23:54:06 -0600

    Define method, #key_for_min_value
    Iterate over a hash and return the key of the key/value pair that holds the lowest value

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,20 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  low_key = []
+  low_value = []
+  name_hash.each do |key, value|
+    if low_value.length == 0
+      low_value.push(value)
+      low_key.push(key)
+    elsif value < low_value[0]
+      low_value.push(value)
+      low_value.shift
+      low_key.push(key)
+      low_key.shift
+    else
+      next
+    end
+  end
+  low_key[0]
 end
